4097be6a242d6af897ec1e73b99e9f0bcccce81d
Alexander Antonison <adantonison@gmail.com>
2022-05-06 14:56:01 -0500

    Update makefile to cover app.py

--- wba-aws-shared-infrastructure/app.py
+++ wba-aws-shared-infrastructure/app.py
@@ -1,7 +1,6 @@
 import aws_cdk as cdk
-
-from aws_cdk_infrastructure.gbbo_s3_bucket import GbboBucket
 from aws_cdk_infrastructure.base_infra_bucket import BaseInfraBucket
+from aws_cdk_infrastructure.gbbo_s3_bucket import GbboBucket
 
 app = cdk.App()
 BaseInfraBucket(app, "base-infra-bucket")
